38fbcb893567301909a3228e3e79799c4a7b1048
ZitroAgario <crspd@i.ua>
2017-04-12 11:26:19 +0300

    Update builder.js

--- builder.js
+++ builder.js
@@ -1,4 +1,4 @@
-(function buildTable(rows, pixels){
+function buildTable(rows, pixels){
 var art=document.getElementById('art');
 for(var i=0;i<rows;i++){
   art.innerHTML+='<div class="row"></div>';
@@ -7,4 +7,4 @@ var row=document.getElementsByClassName('row');
 for(var j=0;j<pixels;j++){
   row[j].innerHTML+='<div class="pixel" onclick="setPixelColour(this)"></div>';
   }
-});
+};
